0214e8fb039b21a44d5c319e61ca4d8d28c01151
vosw1 <vosw1@naver.com>
2024-08-08 20:57:58 +0900

    초기설정

---
+++ src/main/java/com/example/simpe_restapi/SimpeRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpe_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpeRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpeRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpe_restapi/SimpeRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpe_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpeRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
